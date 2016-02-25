319ef1245e98651c1f444b899d0a2d977185105d
Mariano Gappa <mariano@movio.co>
2016-02-25 22:24:41 +1300

    Fixed race condition by looking at array instead of screen

--- main.go
+++ main.go
@@ -12,7 +12,44 @@ func draw_guy(g guy) {
 	termbox.SetCell(g.x, g.y+2, '∩', termbox.Attribute(15), termbox.Attribute(7))
 }
 
-func draw_world() {
+func draw_world(world [30]string) {
+	colour := func(x int, y int, world [30]string) int {
+		switch world[y][x] {
+		case ' ':
+			return 7
+		case '-':
+			return 3
+		case '|':
+			return 3
+		case '!':
+			return 2
+		}
+		return 0
+	}
+	colour(0, 0, world)
+	width, height := 80, 30
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			termbox.SetCell(x, y, rune(world[y][x]), termbox.Attribute(colour(x, y, world)), termbox.Attribute(colour(x, y, world)))
+		}
+	}
+}
+
+func draw_all(g guy, world [30]string) {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	draw_world(world)
+	draw_guy(g)
+
+	termbox.Flush()
+}
+
+type guy struct {
+	x          int
+	y          int
+	jumpCycles int
+}
+
+func main() {
 	world := [30]string{}
 
 	world[0] = "                                                                                "
@@ -46,43 +83,6 @@ func draw_world() {
 	world[28] = "                                                                                "
 	world[29] = "--------------------------------------------------------------------------------"
 
-	colour := func(x int, y int, world [30]string) int {
-		switch world[y][x] {
-		case ' ':
-			return 7
-		case '-':
-			return 3
-		case '|':
-			return 3
-		case '!':
-			return 2
-		}
-		return 0
-	}
-	colour(0, 0, world)
-	width, height := 80, 30
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			termbox.SetCell(x, y, rune(world[y][x]), termbox.Attribute(colour(x, y, world)), termbox.Attribute(colour(x, y, world)))
-		}
-	}
-}
-
-func draw_all(g guy) {
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	draw_world()
-	draw_guy(g)
-
-	termbox.Flush()
-}
-
-type guy struct {
-	x          int
-	y          int
-	jumpCycles int
-}
-
-func main() {
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
@@ -99,36 +99,32 @@ func main() {
 	}()
 
 	redrawRoutine := make(chan guy)
-	go func(r chan guy) {
+	go func(r chan guy, world [30]string) {
 		for {
 			select {
 			case g := <-r:
-				draw_all(g)
+				draw_all(g, world)
 			}
 		}
 
-	}(redrawRoutine)
+	}(redrawRoutine, world)
 
-	go func(t *time.Ticker, gs chan guy) {
+	go func(t *time.Ticker, gs chan guy, world [30]string) {
 		g := guy{x: 10, y: 0, jumpCycles: 0}
 
 		for {
 			select {
 			case <-t.C:
 				if g.jumpCycles == 0 {
-					w, h := termbox.Size()
-					if g.x+(g.y+3)*w < w*h {
-						c := termbox.CellBuffer()[g.x+(g.y+3)*w]
-						if c.Ch == 32 {
+					if g.y <= 26 {
+						if world[g.y+3][g.x] == 32 {
 							g.y++
 							gs <- g
 						}
 					}
 				} else if g.jumpCycles > 0 {
-					w, _ := termbox.Size()
-					if g.x+(g.y-1)*w > 0 {
-						c := termbox.CellBuffer()[g.x+(g.y-1)*w]
-						if c.Ch == 32 {
+					if g.y > 0 {
+						if world[g.y-1][g.x] == 32 {
 							g.y--
 							gs <- g
 						}
@@ -141,34 +137,30 @@ func main() {
 					case termbox.KeyEsc:
 						quit <- "Goodbye!"
 					case termbox.KeyArrowLeft:
-						w, h := termbox.Size()
-						if g.x-1+(g.y+2)*g.x < w*h {
-							c1 := termbox.CellBuffer()[g.x-1+g.y*w]
-							c2 := termbox.CellBuffer()[g.x-1+(g.y+1)*w]
-							c3 := termbox.CellBuffer()[g.x-1+(g.y+2)*w]
+						if g.x > 0 && g.y >= 0 && g.y <= 27 {
+							c1 := world[g.y][g.x-1]
+							c2 := world[g.y+1][g.x-1]
+							c3 := world[g.y+2][g.x-1]
 
-							if g.x > 0 && c1.Ch == 32 && c2.Ch == 32 && c3.Ch == 32 {
+							if c1 == 32 && c2 == 32 && c3 == 32 {
 								g.x--
 								gs <- g
 							}
 						}
 					case termbox.KeyArrowRight:
-						w, h := termbox.Size()
-						if g.x-1+(g.y+2)*g.x < w*h {
-							c1 := termbox.CellBuffer()[g.x+1+g.y*w]
-							c2 := termbox.CellBuffer()[g.x+1+(g.y+1)*w]
-							c3 := termbox.CellBuffer()[g.x+1+(g.y+2)*w]
+						if g.x < 79 && g.y >= 0 && g.y <= 27 {
+							c1 := world[g.y][g.x+1]
+							c2 := world[g.y+1][g.x+1]
+							c3 := world[g.y+2][g.x+1]
 
-							if g.x < 79 && c1.Ch == 32 && c2.Ch == 32 && (c3.Ch == 32 || c3.Ch == 33) {
+							if c1 == 32 && c2 == 32 && (c3 == 32 || c3 == 33) {
 								g.x++
 								gs <- g
 							}
 						}
 					case termbox.KeyArrowUp:
-						w, h := termbox.Size()
-						if g.x+(g.y+3)*w < w*h {
-							c := termbox.CellBuffer()[g.x+(g.y+3)*w]
-							if c.Ch != 32 && g.jumpCycles == 0 {
+						if g.y > 0 {
+							if world[g.y+3][g.x] != 32 && g.jumpCycles == 0 {
 								g.jumpCycles = 5
 							}
 						}
@@ -182,7 +174,7 @@ func main() {
 				quit <- "You WIN!"
 			}
 		}
-	}(ticker, redrawRoutine)
+	}(ticker, redrawRoutine, world)
 
 	m := <-quit
 	termbox.Close()
